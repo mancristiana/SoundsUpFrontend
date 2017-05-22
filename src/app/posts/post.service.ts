ce1eda8d8c404d4f4e698ae6acdace2cffacb9f0
Cristiana Man <man.cristiana1@gmail.com>
2017-05-22 14:23:53 +0200

    Updated Post Service with createPost method

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -3,7 +3,7 @@
  */
 
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
 
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/toPromise';
@@ -17,17 +17,34 @@ import { Post } from '../shared/models/post.model';
 
 @Injectable()
 export class PostService {
+    private headers = new Headers({'Content-Type': 'application/json'});
+    private url = this.apiService.url + '/posts';
 
     constructor(private http: Http,
                 private apiService: ApiService) {}
 
     public getAllPosts(): Observable<Post[]> {
 
-        let endpointUrl = this.apiService.url + '/posts';
-
-        return this.http.get(endpointUrl)
+        return this.http.get(this.url)
             .map((response: Response) => {
                 return response.json() as Post[]
+            })
+            .catch(this.apiService.handleError);
+    }
+
+    public createPost(post: Post): Observable<any> {
+        let body = JSON.stringify(post);
+        let options = new RequestOptions({
+            headers: this.headers
+        });
+
+        return this.http.post(this.url, body, options)
+            .map((response: Response) => {
+                let responseMessage = response.json();
+                console.log(response);
+                console.log(responseMessage);
+
+                return responseMessage;
             })
             .catch(this.apiService.handleError);
     }
