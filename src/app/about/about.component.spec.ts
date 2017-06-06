556ffc836b83ed0838c16dd430a3b3851c4125bf
Cristiana Man <man.cristiana1@gmail.com>
2017-06-06 21:04:57 +0200

    Tests Cleanup

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
 
-describe('About Component', () => {
+describe('AboutComponent', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({declarations: [AboutComponent]});
     });
